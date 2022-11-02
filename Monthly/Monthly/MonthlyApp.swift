89b9c53f41642f41f999ad050c6d8378010fe022
민성홍 <sunghong32@naver.com>
2022-11-02 10:39:57 +0900

    Feat: SwiftUI로 달력 Widget 만들기

---
+++ Monthly/Monthly/MonthlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyApp.swift
+//  Monthly
+//
+//  Created by 민성홍 on 2022/11/01.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
